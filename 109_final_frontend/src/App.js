8249bbc9a8e55558afc2ec2906a486bf28054f0d
jackconnolly21 <jconnolly@college.harvard.edu>
2018-12-11 23:06:22 -0500

    put cluster model on pap

--- 109_final_frontend/src/App.js
+++ 109_final_frontend/src/App.js
@@ -10,82 +10,77 @@ import SuggestionsTable from './components/suggestions_table'
 
 
 class App extends Component {
-  
 
-  
+
+
   handleChange = name => event => {
     return this.setState({name: event.target.value})
 
   }
 
   update_predictions() {
+    // Fetch predictions from network
     fetch('http://127.0.0.1:8000/network-most-likely', {
       method: 'post',
       headers: {
             "Content-Type": "application/json; charset=utf-8",
       },
-      body: JSON.stringify({'songs' : this.state.current_playlist.map(row => {return row.uri})}) 
+      body: JSON.stringify({'songs' : this.state.current_playlist.map(row => {return row.uri})})
       })
       .then(response => response.json())
       .then(data => {
-        this.setState({ vis_data: data.vis_data, suggestion_data: data.suggestion_data }); 
+        this.setState({ network_vis_data: data.vis_data, network_suggestion_data: data.suggestion_data });
       });
+
+      // Fetch predictions from cluster
+      fetch('http://127.0.0.1:8000/cluster-most-likely', {
+        method: 'post',
+        headers: {
+              "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({'songs' : this.state.current_playlist.map(row => {return row.uri})})
+        })
+        .then(response => response.json())
+        .then(data => {
+          this.setState({ cluster_vis_data: data.vis_data, cluster_suggestion_data: data.suggestion_data });
+        });
   }
 
   add_song_handler (row)  {
-      // Get the info for the song trying to be added 
-      // fetch('http://127.0.0.1:8000/song-to-info', {
-      // method: 'post',
-      // headers: {
-      //       "Content-Type": "application/json; charset=utf-8",
-      // },
-      // body: JSON.stringify({'song' : name}) 
-      // })
-      // .then(response => response.json())
-      // .then(data => {
-      //   if (data.success) {
-      //     var new_playlist = this.state.current_playlist.concat(data.data);
-      //     this.setState({current_playlist: new_playlist}); 
-      //     this.update_predictions()
-      //   }
-      //   else {
-      //     alert('Error Adding Song')
-      //   }
-
-      // });
-
-      const new_row = {'artist_name' : row.artist_name, 
-                        'song_name' : row.song_name, 
-                        'album_name' : row.album_name, 
-                        'uri' : row.uri, 
+      const new_row = {'artist_name' : row.artist_name,
+                        'song_name' : row.song_name,
+                        'album_name' : row.album_name,
+                        'uri' : row.uri,
                       };
       const new_playlist = this.state.current_playlist.concat(new_row);
 
-      this.setState({current_playlist: new_playlist}, this.update_predictions); 
+      this.setState({current_playlist: new_playlist}, this.update_predictions);
       // console.log(this.state.current_playlist)
-      // this.update_predictions(); 
+      // this.update_predictions();
 
   }
 
 
   constructor(props) {
     super(props);
-    this.state = { vis_data: [],
-                    suggestion_data: [],  
-                  current_playlist: 
-                    // [{'artist_name': 'Miley Cyrus', 
-                    //   'album_name' : 'The Time of Our Lives', 
-                    //   'song_name' : 'Party In The U.S.A.', 
-                    //   'uri' : 'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'}], 
-                    [{'artist_name': 'Future', 
-                      'album_name' : 'FUTURE', 
-                      'song_name' : 'Mask Off', 
-                      'uri' : 'spotify:track:0VgkVdmE4gld66l8iyGjgx'}],
-                      // [{'artist_name': 'John Mayer', 
+    this.state = { network_vis_data: [],
+                    network_suggestion_data: [],
+                    cluster_vis_data: [],
+                    cluster_suggestion_data: [],
+                  current_playlist:
+                    [{'artist_name': 'Miley Cyrus',
+                      'album_name' : 'The Time of Our Lives',
+                      'song_name' : 'Party In The U.S.A.',
+                      'uri' : 'spotify:track:5Q0Nhxo0l2bP3pNjpGJwV1'}],
+                    // [{'artist_name': 'Future',
+                    //   'album_name' : 'FUTURE',
+                    //   'song_name' : 'Mask Off',
+                    //   'uri' : 'spotify:track:0VgkVdmE4gld66l8iyGjgx'}],
+                      // [{'artist_name': 'John Mayer',
                       // 'song_name': 'Waiting On the World to Change',
-                      // 'album_name': 'Continuum', 
-                      // 'uri' : 'spotify:track:5imShWWzwqfAJ9gXFpGAQh'}], 
-                  add_name: '', 
+                      // 'album_name': 'Continuum',
+                      // 'uri' : 'spotify:track:5imShWWzwqfAJ9gXFpGAQh'}],
+                  add_name: '',
                   };
 
   }
@@ -93,19 +88,19 @@ class App extends Component {
 
 
   componentDidMount(){
-    this.update_predictions(); 
+    this.update_predictions();
   }
 
   render() {
 
     return (
-      <div style={{backgroundColor: '#DCDCDC', height: '100vh'}}> 
+      <div style={{backgroundColor: '#DCDCDC', height: '100vh'}}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" color="inherit">
             CS109a Playlist Builder
           </Typography>
-          <div style={{flexGrow: 1}}></div> 
+          <div style={{flexGrow: 1}}></div>
           <div style={{width: '250px'}}>
           </div>
         </Toolbar>
@@ -114,22 +109,31 @@ class App extends Component {
       <Grid item xs={12} style={{paddingTop: '150px'}}>
         <Grid container  justify="center" spacing={32}>
           <Grid item style={{width: '600px'}}>
-            <CustomTable data={this.state.current_playlist} /> 
+            <CustomTable data={this.state.current_playlist} />
           </Grid>
-          <Grid item style={{width: '800px', height: '500px'}}> 
-            <SuggestionsTable 
-                data={this.state.suggestion_data} 
+          <Grid item style={{width: '800px', height: '500px'}}>
+            <SuggestionsTable
+                data={this.state.network_suggestion_data}
                 handler={this.add_song_handler.bind(this)}
                 refresh={this.update_predictions.bind(this)}
                 name="Network"
+                score_name="Score"
             />
-          </Grid> 
+            <div style={{height: '10px'}}> </div>
+            <SuggestionsTable
+                data={this.state.cluster_suggestion_data}
+                handler={this.add_song_handler.bind(this)}
+                refresh={this.update_predictions.bind(this)}
+                name="Cluster Model"
+                score_name="Distance"
+            />
+          </Grid>
           <Grid item>
-            <SuggestedVis data={this.state.vis_data} />
+            <SuggestedVis data={this.state.network_vis_data} />
           </Grid>
         </Grid>
       </Grid>
-    </div> 
+    </div>
     );
   }
 }
--- cs109_final_backend/cs109_final_backend/views.py
+++ cs109_final_backend/cs109_final_backend/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, JsonResponse
 import pickle
 import numpy as np
+import pandas as pd
 import json
 from rest_framework.decorators import api_view
 
@@ -20,60 +21,58 @@ with open('cs109_final_backend/network_files/track_to_artist_album.pickle', 'rb'
 	TRACK_TO_ARTIST_ALBUM = pickle.load(f)
 
 with open('cs109_final_backend/cluster_files/uris_10.pickle', 'rb') as f:
-    URIS_SET = pickle.load(f)
+	URIS_SET = pickle.load(f)
 
-with open('cs109_final_backend/cluster_files/audio_features.pickle', 'rb') as f:
-    AUDIO_FEATURES = pickle.load(f)
+AUDIO_FEATURES = pd.read_pickle('cs109_final_backend/cluster_files/audio_features.pickle')
 
 with open('cs109_final_backend/cluster_files/song_to_cluster.pickle', 'rb') as f:
-    SONG_TO_CLUSTER = pickle.load(f)
+	SONG_TO_CLUSTER = pickle.load(f)
 
 with open('cs109_final_backend/cluster_files/cluster_to_songs.pickle', 'rb') as f:
-    CLUSTER_TO_SONGS = pickle.load(f)
+	CLUSTER_TO_SONGS = pickle.load(f)
 
 def make_audio_dict(audio_features):
-	audio_df = pd.DataFrame(audio_features)
-	audio_scaled = StandardScaler().fit_transform(audio_df)
+	audio_scaled = StandardScaler().fit_transform(audio_features)
 
 	audio_dict = {}
-	for i in range(len(uris)):
-	    audio_dict[uris[i]] = audio_scaled[i]
+	for i in range(len(URIS_SET)):
+		audio_dict[URIS_SET[i]] = audio_scaled[i]
 
 	return audio_dict
 
 AUDIO_DICT = make_audio_dict(AUDIO_FEATURES)
 
 def sample_from_list(playlist_songs, network, u_to_s, num_samples=4000, as_names=True):
-    # if not uri :
-    #     playlist_songs = [s_to_u[s] for s in playlist_songs]
-    # print(playlist_songs)
-    all_samples = np.array([])
-    for song_uri in playlist_songs:
-        sample = np.random.choice(network[song_uri]['songs'], num_samples, p=network[song_uri]['counts'])
-        all_samples = np.append(all_samples, sample)
+	# if not uri :
+	#     playlist_songs = [s_to_u[s] for s in playlist_songs]
+	# print(playlist_songs)
+	all_samples = np.array([])
+	for song_uri in playlist_songs:
+		sample = np.random.choice(network[song_uri]['songs'], num_samples, p=network[song_uri]['counts'])
+		all_samples = np.append(all_samples, sample)
 
-    unique, counts = np.unique(all_samples, return_counts=True)
+	unique, counts = np.unique(all_samples, return_counts=True)
 
-    counts = 100 * counts.astype(float) / np.sum(counts)
-    # print (counts)
-    counted_samples = zip(unique, counts)
-    counted_samples = [sample for sample in counted_samples if sample[0] not in playlist_songs]
-    counted_samples = sorted(counted_samples, key=lambda x: x[1], reverse=True)
+	counts = 100 * counts.astype(float) / np.sum(counts)
+	# print (counts)
+	counted_samples = zip(unique, counts)
+	counted_samples = [sample for sample in counted_samples if sample[0] not in playlist_songs]
+	counted_samples = sorted(counted_samples, key=lambda x: x[1], reverse=True)
 
-    num_to_return = min(15, len(counted_samples))
+	num_to_return = min(15, len(counted_samples))
 
-    return counted_samples[:num_to_return]
+	return counted_samples[:num_to_return]
 
 
 def convert_to_viz_data(suggestions):
-	vis_data = [{'x': URI_TO_SONG[s[0]][:20], 'y': round(float(s[1]), 3)} for s in suggestions]
+	vis_data = [{'x': URI_TO_SONG[s[0]][:20], 'y': round(float(s[1]), 3)} for s in suggestions if s[0] in URI_TO_SONG]
 
 	suggestion_data = [{'song_name': URI_TO_SONG[s[0]],
 						'artist_name' : TRACK_TO_ARTIST_ALBUM[s[0]]['artist'],
 						'album_name': TRACK_TO_ARTIST_ALBUM[s[0]]['album'],
 						'score' : round(float(s[1]),3),
 						'uri' : s[0]
-						} for s in suggestions ]
+						} for s in suggestions if s[0] in URI_TO_SONG]
 
 	num_suggestions = min(5, len(suggestion_data))
 	suggestion_data = suggestion_data[:num_suggestions]
@@ -111,32 +110,33 @@ def network_most_likely(request):
 def cluster_most_likely(request):
 
 	def predict_knn(playlist, song_to_cluster, cluster_to_songs, audio_dict):
-        clusters = [song_to_cluster[song] for song in playlist if song in song_to_cluster]
-        unique, counts = np.unique(clusters, return_counts=True)
-        max_cluster_id = unique[np.argmax(counts)]
-        max_cluster = cluster_to_song[max_cluster_id]
+		clusters = [song_to_cluster[song] for song in playlist if song in song_to_cluster]
+		unique, counts = np.unique(clusters, return_counts=True)
+		max_cluster_id = unique[np.argmax(counts)]
+		max_cluster = cluster_to_songs[max_cluster_id]
+		np.random.shuffle(max_cluster)
 
-        avg_feat = get_average_features(playlist)
-        distances = [(uri, distance(avg_feat, audio_dict[uri])) for uri in max_cluster]
-        distances.sort(key=lambda tup: tup[1])
+		avg_feat = get_average_features(playlist, audio_dict)
+		distances = [(uri, distance(avg_feat, audio_dict[uri])) for uri in max_cluster if uri not in playlist]
+		distances.sort(key=lambda tup: tup[1])
 
 		num_to_return = min(15, len(distances))
-	    return distances[:num_to_return]
+		return [s for s in distances[:num_to_return]]
 
-    def get_average_features(playlist):
-        average_features = None
-        for uri in playlist:
-            features = audio_dict[uri]
-            if average_features is None:
-                average_features = features
-            else:
-                average_features = average_features + features
-        average_features = average_features / len(playlist)
-        return average_features
+	def get_average_features(playlist, audio_dict):
+		average_features = None
+		for uri in playlist:
+			features = audio_dict[uri]
+			if average_features is None:
+				average_features = features
+			else:
+				average_features = average_features + features
+		average_features = average_features / len(playlist)
+		return average_features
 
-    def distance(audio1, audio2):
-        distance = np.sqrt(np.sum((audio1 - audio2) ** 2.0))
-        return distance
+	def distance(audio1, audio2):
+		distance = np.sqrt(np.sum((audio1 - audio2) ** 2.0))
+		return distance
 
 	songs_with_dists = predict_knn(request.data['songs'], SONG_TO_CLUSTER,
 									CLUSTER_TO_SONGS, AUDIO_DICT)
@@ -144,16 +144,3 @@ def cluster_most_likely(request):
 	to_return = convert_to_viz_data(songs_with_dists)
 
 	return JsonResponse(to_return, safe=False)
-
-
-# @api_view(['POST'])
-# def song_name_to_info(request):
-# 	song_name = request.data['song']
-# 	if song_name in SONGS_TO_URI :
-# 		song_uri = SONGS_TO_URI[song_name]
-# 		album_name = TRACK_TO_ARTIST_ALBUM[song_uri]['album']
-# 		artist_name = TRACK_TO_ARTIST_ALBUM[song_uri]['artist']
-# 		return JsonResponse({'success' : True, 'data' : {'song_name' : song_name, 'album_name': album_name, 'artist_name': artist_name}})
-
-# 	else :
-# 		return JsonResponse({'success': False})
